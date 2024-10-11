73d4adb2158c9d46c8bc5fa83b6a2115e1c262a4
ethanmacleod163 <ethandavidfrancis@gmail.com>
2024-10-11 23:28:30 +1300

    Removed Failing line

--- portfolio/src/common/PDFViewer.tsx
+++ portfolio/src/common/PDFViewer.tsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 const documents: string[] = ["/documents/Dissertation.pdf", "/documents/LiteratureReview.pdf", "/documents/ProjectProposal.pdf"];
 
 const PDFViewer = () => {
-    const [selectedDocument, setDocument] = useState(0);
-    setDocument(0);
+    // const [selectedDocument, setDocument] = useState(0);
 
     return (
         <div>
-            <object data={documents[selectedDocument]} className="w-full h-[95vh]" type="application/pdf" />
+            <object data={documents[0]} className="w-full h-[95vh]" type="application/pdf" />
         </div>
     );
 };
